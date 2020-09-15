f7405419051dc361f2d5b473977320b544fb2b04
HiroakiKawasaki <kawa_307815@icloud.com>
2020-09-15 19:01:05 +0900

    ユーザー認証の記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'kawasaki' && password == '1515'
+    end
+  end
 end
