76a5b1052ecf6b0ec208af4eef7de71715a9e216
HiroakiKawasaki <kawa_307815@icloud.com>
2020-09-25 16:22:14 +0900

    itemsコントローラーの導入
    ルーティングの記述の修正も行いました。

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,14 @@
 class UserController < ApplicationController
 
-  private
+  def new
+    @users = User.all
+  end
 
-  
+  def edit
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
