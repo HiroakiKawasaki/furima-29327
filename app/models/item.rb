a7f7353dda9851977f3ce3719ed46685be587b33
HiroakiKawasaki <kawa_307815@icloud.com>
2020-09-29 19:02:20 +0900

    ActiveStorageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
-
   belongs_to :user
+  has_one_attached :image
+
+  validates :name,            presence: true
+  validates :price,           presence: true
+  validates :category_id,     presence: true
+  validates :status_id,       presence: true
+  validates :delivery_fee_id, presence: true
+  validates :area_id,         presence: true
+  validates :date_id,         presence: true
+ 
 end
