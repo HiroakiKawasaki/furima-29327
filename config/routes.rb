df5d58a26d3df322f35ac6811ddac43a8f46ea49
HiroakiKawasaki <kawa_307815@icloud.com>
2020-09-29 15:57:00 +0900

    データベースの作成とモデルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'items#index'
   
   resources :user
+  resources :items
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
