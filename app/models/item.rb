cedb8132547097d70adb1e8d038a16f78e41b1cd
HiroakiKawasaki <kawa_307815@icloud.com>
2020-10-08 09:39:24 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,16 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :name,     presence: true
-  validates :price,    presence: true, format: { with: /\A[0-9]+\z/}
-  validates :category, presence: true
-  validates :status,   presence: true
-  validates :fee,      presence: true
-  validates :area,     presence: true
-  validates :arrival, presence: true
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :explanation
+    validates :category, numericality: { greater_than: 1 }
+    validates :status, numericality: { greater_than: 1 }
+    validates :fee , numericality: { greater_than: 1 }
+    validates :area, numericality: { greater_than: 1 }
+    validates :arrival, numericality: { greater_than: 1 }
+    validates :price, format: { with: /\A[0-9]+\z/, message: 'Price Half-width number'}, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+  end
 end
