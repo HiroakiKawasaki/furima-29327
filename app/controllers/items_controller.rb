6a0f3d942d4eea6e68de71286612ccac865fc7d6
HiroakiKawasaki <kawa_307815@icloud.com>
2020-10-13 18:12:50 +0900

    条件分岐の修正とエラーハンドリングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,12 +30,12 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destro
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    if item.update(item_params)
+    @item = Item.find(params[:id])
+    # item.update(item_params)
+    if @item.update(item_params)
       redirect_to item_path
     else
-      render :edit
+      render "edit"
     end
   end
 
