30b2412ec654ee975e124de39ab5757b38725204
HiroakiKawasaki <kawa_307815@icloud.com>
2020-10-14 16:46:45 +0900

    バリデーションとアソシエーションの記述

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,2 +1,11 @@
 class Delivery < ApplicationRecord
+  belongs_to :purchase
+
+  with_options presence: true do
+    validates :post
+    validates :prefecture_id
+    validates :cities
+    validates :address
+    validates :phone_number
+  end
 end
--- db/migrate/20201014064418_create_deliveries.rb
+++ db/migrate/20201014064418_create_deliveries.rb
@@ -1,6 +1,12 @@
 class CreateDeliveries < ActiveRecord::Migration[6.0]
   def change
     create_table :deliveries do |t|
+      t.string  :post,          null: false
+      t.integer :prefecture_id, null: false
+      t.string  :cities,        null: false
+      t.string  :address,       null: false
+      t.string  :building_name
+      t.string  :phone_number,  null: false
 
       t.timestamps
     end
