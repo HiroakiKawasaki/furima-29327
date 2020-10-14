d15b65e8547585368ff4bf40895085b6b27e83e4
HiroakiKawasaki <kawa_307815@icloud.com>
2020-10-14 17:55:11 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   
   resources :users
   resources :items
+  resources :purchases
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
